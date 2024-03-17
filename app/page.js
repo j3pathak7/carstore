6877983946393b69005673fcc5cc9a3970817d49
Jyotirmoy Pathak <jyotirmoypathak.37@gmail.com>
2024-03-17 14:57:20 +0530

    i

--- app/page.js
+++ app/page.js
@@ -1,21 +1,19 @@
 import HeroSlider from "./components/HeroSlider";
-import AboutUs from "./components/AboutUs";
+
 import CarCards from "./components/CarCardAll";
 import Address from "./components/Address";
 import SoldCars from "./components/SoldCars";
-import CarCardsDemo from "./components/CarAllDemo";
 
 export default function Home() {
   return (
     <main className="bg-white">
       <div className=" ">
-        {/* <Hero /> */}
         <CarCards />
-        {/* <CarCardsDemo /> */}
+
         <HeroSlider />
         <div className="lg:max-w-[960px] xl:max-w-[1140px] 2xl:max-w-[1440px] mx-auto px-2"></div>
         <Address />
-        {/* <AboutUs /> */}
+
         <SoldCars />
       </div>
     </main>
