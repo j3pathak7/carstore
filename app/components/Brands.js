b60c4781b4af94b7e03dea41182ab8953f54aa8e
Jyotirmoy Pathak <jyotirmoypathak.37@gmail.com>
2024-03-17 17:13:32 +0530

    added brands

--- app/components/Brands.js
+++ app/components/Brands.js
@@ -1,11 +1,60 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
 
 const Brands = () => {
+  const [showAllBrands, setShowAllBrands] = useState(false);
+
+  const brands = [
+    { name: "hyundai", alt: "Hyundai Logo" },
+    { name: "suzuki", alt: "Suzuki Logo" },
+    { name: "tata", alt: "Tata Logo" },
+    { name: "ford", alt: "Ford Logo" },
+    { name: "honda", alt: "Honda Logo" },
+    { name: "volkswagen", alt: "Volkswagen Logo" },
+    { name: "chevrolet", alt: "Chevrolet Logo" },
+    { name: "kia", alt: "Kia Logo" },
+    { name: "mercedes", alt: "Mercedes Logo" },
+    { name: "renault", alt: "Renault Logo" },
+    { name: "toyota", alt: "Toyota Logo" },
+    { name: "bmw", alt: "BMW Logo" },
+  ];
+
+  const visibleBrands = showAllBrands ? brands : brands.slice(0, 6);
+
   return (
     <div>
-      <div>Brands</div>
+      <h1 className="text-3xl font-bold mb-8 flex justify-center">
+        {" "}
+        Choose by Brand:{" "}
+      </h1>
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4 mx-2 sm:mx-10 md:mx-20 lg:mx-40 mb-8">
+        {visibleBrands.map((brand, index) => (
+          <div
+            key={index}
+            className="border p-2 sm:p-4 flex justify-center items-center border-teal-200"
+          >
+            <Image
+              src={`/Brands/${brand.name}.png`}
+              width={500}
+              height={500}
+              alt={brand.alt}
+            />
+          </div>
+        ))}
+      </div>
+      {!showAllBrands && (
+        <div className="flex justify-center">
+          <button
+            onClick={() => setShowAllBrands(true)}
+            className="btn bg-white  text-teal-600 mb-5 rounded mb-32"
+          >
+            See More
+          </button>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Brands
+export default Brands;
--- app/page.js
+++ app/page.js
@@ -3,13 +3,14 @@ import HeroSlider from "./components/HeroSlider";
 import CarCards from "./components/CarCardAll";
 import Address from "./components/Address";
 import SoldCars from "./components/SoldCars";
+import Brands from "./components/Brands";
 
 export default function Home() {
   return (
     <main className="bg-white">
       <div className=" ">
         <CarCards />
-
+        <Brands />
         <HeroSlider />
         <div className="lg:max-w-[960px] xl:max-w-[1140px] 2xl:max-w-[1440px] mx-auto px-2"></div>
         <Address />
