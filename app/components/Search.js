9046febf42815e477cd8ce7bd9942b5cc98254f9
Jyotirmoy Pathak <jyotirmoypathak.37@gmail.com>
2024-03-20 04:41:57 +0530

    again fixed search

--- app/components/Search.js
+++ app/components/Search.js
@@ -16,7 +16,7 @@ const Search = () => {
 
   return (
     <div className="flex justify-center m-8">
-      <form onSubmit={handleSubmit} className="w-full md:max-w-md">
+      <form onSubmit={handleSubmit} className="w-full max-w-md">
         <div className="flex flex-col md:flex-row items-stretch md:items-center rounded-t-md">
           <input
             type="text"
@@ -25,12 +25,14 @@ const Search = () => {
             onChange={handleSearchChange}
             className="flex-grow px-4 py-2 focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:ring-opacity-50 rounded-t-md md:rounded-l-md sm:text-sm"
           />
-          <button
-            type="submit"
-            className="w-full md:w-auto mt-4 md:mt-0 px-4 py-2 text-white bg-cyan-500 hover:bg-cyan-700 rounded-b-md md:rounded-l-none rounded-r-md focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:ring-opacity-50"
-          >
-            Search
-          </button>
+          <Link href={`/search/${searchTerm}`}>
+            <button
+              type="submit"
+              className="w-full md:w-auto mt-4 md:mt-0 px-4 py-2 text-white bg-cyan-500 hover:bg-cyan-700 rounded-b-md md:rounded-l-none rounded-r-md focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:ring-opacity-50"
+            >
+              Search
+            </button>
+          </Link>
         </div>
       </form>
     </div>
