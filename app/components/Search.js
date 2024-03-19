86dd5b31697ebfc454481de584c38d4f1976c93a
Jyotirmoy Pathak <jyotirmoypathak.37@gmail.com>
2024-03-20 04:35:11 +0530

    fixed search

--- app/components/Search.js
+++ app/components/Search.js
@@ -16,23 +16,21 @@ const Search = () => {
 
   return (
     <div className="flex justify-center m-8">
-      <form onSubmit={handleSubmit} className="w-full max-w-md">
-        <div className="flex items-center rounded-t-md">
+      <form onSubmit={handleSubmit} className="w-full md:max-w-md">
+        <div className="flex flex-col md:flex-row items-stretch md:items-center rounded-t-md">
           <input
             type="text"
             placeholder="Search..."
             value={searchTerm}
             onChange={handleSearchChange}
-            className="flex-grow px-4 py-2 focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:ring-opacity-50 rounded-l-md sm:text-sm"
+            className="flex-grow px-4 py-2 focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:ring-opacity-50 rounded-t-md md:rounded-l-md sm:text-sm"
           />
-          <Link href={`/search/${searchTerm}`}>
-            <button
-              type="submit"
-              className="px-4 py-2 text-white bg-cyan-500 hover:bg-cyan-700 rounded-r-md focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:ring-opacity-50"
-            >
-              Search
-            </button>
-          </Link>
+          <button
+            type="submit"
+            className="w-full md:w-auto mt-4 md:mt-0 px-4 py-2 text-white bg-cyan-500 hover:bg-cyan-700 rounded-b-md md:rounded-l-none rounded-r-md focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:ring-opacity-50"
+          >
+            Search
+          </button>
         </div>
       </form>
     </div>
