a3454c41323ea60975ed5ae824a3c0beb6f15827
Jyotirmoy Pathak <jyotirmoypathak.37@gmail.com>
2024-03-20 03:08:57 +0530

    refix

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -1,19 +1,12 @@
 import { FaCarSide } from "react-icons/fa";
 import React from "react";
-import Image from "next/image";
 
 const Loading = () => {
   return (
     <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-gradient-to-r from-teal-950 to-black z-50">
-      {/* <div className="circle-animation"> */}
-      {/* <FaCarSide size={50} className="car" /> */}
-      <Image
-        src="/loading.gif"
-        width={500}
-        height={500}
-        alt="Picture of the author"
-      />
-      {/* </div> */}
+      <div className="circle-animation">
+        <FaCarSide size={50} className="car" />
+      </div>
     </div>
   );
 };
