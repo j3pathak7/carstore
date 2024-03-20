989b412ed49a58c817136a97f5a2a867f4039a8a
Jyotirmoy Pathak <jyotirmoypathak.37@gmail.com>
2024-03-20 16:44:33 +0530

    m

--- app/components/Search.js
+++ app/components/Search.js
@@ -26,7 +26,7 @@ const Search = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <div className="relative isolate overflow-hidden px-6 pt-8 text-center sm:px-16 sm:shadow-sm">
+      <div className="relative isolate overflow-hidden px-6 py-6 text-center sm:px-16 sm:shadow-sm">
         <label
           className="mx-auto relative bg-white min-w-sm max-w-2xl flex flex-row items-center justify-center border py-2 px-2 rounded-2xl gap-2 shadow-2xl focus-within:border-gray-300"
           htmlFor="search-bar"
