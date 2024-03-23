7f882de3b22230a1da32cc6ed354b14c106cc5e1
Jyotirmoy Pathak <jyotirmoypathak.37@gmail.com>
2024-03-23 22:16:52 +0530

    f

--- app/page.js
+++ app/page.js
@@ -1,4 +1,3 @@
-"use client";
 import HeroSlider from "./components/HeroSlider";
 import Address from "./components/Address";
 import SoldCars from "./components/SoldCars";
@@ -8,12 +7,10 @@ import Image from "next/image";
 import CarTypes from "./components/CarTypes";
 import Search from "./components/Search";
 
-
 export default function Home() {
   return (
     <main className="">
       <div className="">
-
         <Search />
         <div className=" my-8 rounded-2xl w-40 md:w-80 flex justify-center mx-auto p-8 bg-white shadow-2xl shadow-cyan-500">
           <Image
@@ -21,6 +18,7 @@ export default function Home() {
             width={200}
             height={200}
             alt="Picture of the author"
+            loading="lazy"
           />
         </div>
         <div>
