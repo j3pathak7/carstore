093f92241d1edc068b2853ec727b54b66627252d
Jyotirmoy Pathak <jyotirmoypathak.37@gmail.com>
2024-03-24 22:18:38 +0530

    fix

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -4,8 +4,17 @@ import React from "react";
 const Loading = () => {
   return (
     <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-gradient-to-r from-cyan-950 to-black z-50">
-      {/* Remove circle-animation div if not needed */}
-      <div className="loader"></div>
+      <div
+        className="self-center text-2xl lg:text-4xl text-cyan-400"
+        style={{ textTransform: "uppercase" }}
+      >
+        <span style={{ fontSize: "1.5em" }}>T</span>
+        <span style={{ fontSize: "1em", marginRight: "0.3em" }}>HE</span>
+        <span style={{ fontSize: "1.5em" }}>C</span>
+        <span style={{ fontSize: "1em", marginRight: "0.3em" }}>AR</span>
+        <span style={{ fontSize: "1.5em" }}>S</span>
+        <span style={{ fontSize: "1em", marginRight: "0.3em" }}>TORE</span>
+      </div>
     </div>
   );
 };
