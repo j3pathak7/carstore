74977ebd985278fe3a7bd333e086c50fbed23ddc
Jyotirmoy Pathak <jyotirmoypathak.37@gmail.com>
2024-03-26 00:43:51 +0530

    fix

--- app/components/CarCardAll.js
+++ app/components/CarCardAll.js
@@ -100,104 +100,109 @@ const Cars = () => {
     sortByPrice !== null;
 
   return (
-    <div className="container mx-auto px-8">
-      <h1 className="pb-4 heading text-cyan-500 pt-2">
-        <span className="text-cyan-700">List of</span> Cars
-      </h1>
+    <div>
       <Search />
-      <div className="md:flex justify-between items-center mb-4 md:h-10">
-        <div className="flex">
-          <button
-            className="mr-4 bg-gray-200 hover:bg-gray-300 px-3 py-1 rounded"
-            onClick={() => setShowFilter(!showFilter)}
-          >
-            Filter
-          </button>
-          <button
-            className="bg-gray-200 hover:bg-gray-300 px-3 py-1 rounded"
-            onClick={() => setShowSort(!showSort)}
-          >
-            Sort
-          </button>
-        </div>
-        {showFilter && (
-          <div className="flex flex-wrap gap-4 justify-center">
-            <div className="m-4">
-              <label htmlFor="brandFilter" className="mr-2 text-cyan-50">
-                Brand:
-              </label>
-              <select
-                id="brandFilter"
-                className="px-3 py-2 border border-gray-300 rounded-md bg-white"
-                onChange={handleBrandFilterChange}
-                value={filteredBrand || ""}
-              >
-                <option value="">All Brands</option>
-                <option value="Toyota">Toyota</option>
-                <option value="Maruti">Maruti</option>
-                <option value="Hyundai">Hyundai</option>
-                <option value="Ford">Ford</option>
-              </select>
-            </div>
-            <div className=" flex justify-center  items-center md:m-4">
-              <label htmlFor="transmissionFilter" className="mr-2 text-cyan-50">
-                Transmission:
-              </label>
-              <select
-                id="transmissionFilter"
-                className="px-3 py-2 border border-gray-300 rounded-md bg-white"
-                onChange={handleTransmissionFilterChange}
-                value={filteredTransmission || ""}
-              >
-                <option value="">All Transmissions</option>
-                <option value="Manual">Manual</option>
-                <option value="Automatic">Automatic</option>
-              </select>
-            </div>
-          </div>
-        )}
-        {showSort && (
-          <div className="m-4 flex justify-center items-center">
-            <label htmlFor="priceSort" className="mr-2 text-cyan-50">
-              Price:
-            </label>
-            <select
-              id="priceSort"
-              className="px-3 py-2 border border-gray-300 rounded-md bg-white"
-              onChange={handlePriceSortChange}
-              value={sortByPrice || ""}
+      <div className="container mx-auto px-8">
+        <h1 className="pb-4 heading text-cyan-500 pt-2">
+          <span className="text-cyan-700">List of</span> Cars
+        </h1>
+        <div className="md:flex justify-between items-center mb-4 md:h-10">
+          <div className="flex">
+            <button
+              className="mr-4 bg-gray-200 hover:bg-gray-300 px-3 py-1 rounded"
+              onClick={() => setShowFilter(!showFilter)}
             >
-              <option value="">No Sorting</option>
-              <option value="asc">Low to High</option>
-              <option value="desc">High to Low</option>
-            </select>
-          </div>
-        )}
-      </div>
-      {showNoCarsMessage && carList.length === 0 && (
-        <div className="m-8">
-          <p className="text-red-600 text-center mx-8">
-            Sorry, we couldn&apos;t find any cars as per your filters. Would you
-            like to adjust your criteria or explore other options?
-          </p>
-          <div className="flex justify-center m-8">
-            <button className="btn" onClick={resetFilters}>
-              Reset Filters
+              Filter
+            </button>
+            <button
+              className="bg-gray-200 hover:bg-gray-300 px-3 py-1 rounded"
+              onClick={() => setShowSort(!showSort)}
+            >
+              Sort
             </button>
           </div>
+          {showFilter && (
+            <div className="flex flex-wrap gap-4 justify-center">
+              <div className="m-4">
+                <label htmlFor="brandFilter" className="mr-2 text-cyan-50">
+                  Brand:
+                </label>
+                <select
+                  id="brandFilter"
+                  className="px-3 py-2 border border-gray-300 rounded-md bg-white"
+                  onChange={handleBrandFilterChange}
+                  value={filteredBrand || ""}
+                >
+                  <option value="">All Brands</option>
+                  <option value="Toyota">Toyota</option>
+                  <option value="Maruti">Maruti</option>
+                  <option value="Hyundai">Hyundai</option>
+                  <option value="Ford">Ford</option>
+                </select>
+              </div>
+              <div className=" flex justify-center  items-center md:m-4">
+                <label
+                  htmlFor="transmissionFilter"
+                  className="mr-2 text-cyan-50"
+                >
+                  Transmission:
+                </label>
+                <select
+                  id="transmissionFilter"
+                  className="px-3 py-2 border border-gray-300 rounded-md bg-white"
+                  onChange={handleTransmissionFilterChange}
+                  value={filteredTransmission || ""}
+                >
+                  <option value="">All Transmissions</option>
+                  <option value="Manual">Manual</option>
+                  <option value="Automatic">Automatic</option>
+                </select>
+              </div>
+            </div>
+          )}
+          {showSort && (
+            <div className="m-4 flex justify-center items-center">
+              <label htmlFor="priceSort" className="mr-2 text-cyan-50">
+                Price:
+              </label>
+              <select
+                id="priceSort"
+                className="px-3 py-2 border border-gray-300 rounded-md bg-white"
+                onChange={handlePriceSortChange}
+                value={sortByPrice || ""}
+              >
+                <option value="">No Sorting</option>
+                <option value="asc">Low to High</option>
+                <option value="desc">High to Low</option>
+              </select>
+            </div>
+          )}
         </div>
-      )}
-      {/* Cars Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 text-cyan-800">
-        {currentPageCars.map((car) => (
-          <CarCard key={car.id} car={car} />
-        ))}
+        {showNoCarsMessage && carList.length === 0 && (
+          <div className="m-8">
+            <p className="text-red-600 text-center mx-8">
+              Sorry, we couldn&apos;t find any cars as per your filters. Would
+              you like to adjust your criteria or explore other options?
+            </p>
+            <div className="flex justify-center m-8">
+              <button className="btn" onClick={resetFilters}>
+                Reset Filters
+              </button>
+            </div>
+          </div>
+        )}
+        {/* Cars Grid */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 text-cyan-800">
+          {currentPageCars.map((car) => (
+            <CarCard key={car.id} car={car} />
+          ))}
+        </div>
+        <Pagination
+          totalPages={totalPages}
+          currentPage={pageNumber}
+          changePage={changePage}
+        />
       </div>
-      <Pagination
-        totalPages={totalPages}
-        currentPage={pageNumber}
-        changePage={changePage}
-      />
     </div>
   );
 };
--- app/components/Search.js
+++ app/components/Search.js
@@ -1,7 +1,8 @@
 "use client";
 import Link from "next/link";
 import React, { useState } from "react";
-import { FiSearch } from "react-icons/fi"; // Assuming you're using React Icons library
+import { FiSearch } from "react-icons/fi";
+import Image from "next/image";
 
 const Search = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -12,7 +13,6 @@ const Search = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Redirect to search results page with search term
     if (searchTerm.trim() !== "") {
       window.location.href = `/search/${searchTerm}`;
     }
@@ -25,47 +25,61 @@ const Search = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="relative isolate overflow-hidden px-6 py-6 text-center sm:px-16 sm:shadow-sm">
-        <label
-          className="mx-auto relative bg-white min-w-sm max-w-2xl flex flex-row items-center justify-center border py-2 px-2 rounded-2xl gap-2 shadow-2xl focus-within:border-gray-300"
-          htmlFor="search-bar"
-        >
-          <input
-            id="search-bar"
-            placeholder="Search for a car"
-            name="q"
-            value={searchTerm}
-            onChange={handleSearchChange}
-            onKeyPress={handleKeyPress}
-            className="px-2 w-full rounded-md flex-1 outline-none bg-white sm:h-10"
-            required=""
-            type="text"
+    <div className="relative flex flex-col justify-center overflow-hidden">
+      <div className="relative h-1/2">
+        <div className="absolute inset-0">
+          <Image
+            src="/searchCar.jpeg"
+            alt="Car Image"
+            layout="fill"
+            objectFit="cover"
+            priority
           />
-          {/* React icon for small devices */}
-          <div className="md:hidden">
-            <Link href={`/search/${searchTerm}`}>
-              <FiSearch className="text-gray-600 h-6 w-6 ml-2 cursor-pointer" />
-            </Link>
-          </div>
-          {/* Button for larger screens */}
-          <div className="hidden md:block">
-            <Link href={`/search/${searchTerm}`}>
-              <button
-                type="submit"
-                className="px-6 btn border-black text-white fill-white active:scale-95 duration-100 border will-change-transform overflow-hidden relative rounded-xl transition-all"
-              >
-                <div className="flex items-center transition-all opacity-1">
-                  <span className="text-sm font-semibold whitespace-nowrap truncate mx-auto">
-                    Search
-                  </span>
-                </div>
-              </button>
-            </Link>
-          </div>
-        </label>
+          <div className="absolute inset-0 bg-black opacity-50"></div>
+        </div>
+        <div className="relative z-10 flex justify-center items-center h-full px-6 py-6">
+          <form onSubmit={handleSubmit} className="max-w-2xl w-full">
+            <label
+              className="relative bg-white flex flex-row items-center justify-center border py-2 px-2 rounded-full gap-2 shadow-2xl focus-within:border-gray-300"
+              htmlFor="search-bar"
+            >
+              <input
+                id="search-bar"
+                placeholder="Search for a car"
+                name="q"
+                value={searchTerm}
+                onChange={handleSearchChange}
+                onKeyPress={handleKeyPress}
+                className="px-2 w-full rounded-md flex-1 outline-none bg-white sm:h-10"
+                required
+                type="text"
+              />
+              {/* React icon for small devices */}
+              <div className="md:hidden">
+                <Link href={`/search/${searchTerm}`}>
+                  <FiSearch className="text-gray-600 h-6 w-6 ml-2 cursor-pointer" />
+                </Link>
+              </div>
+              {/* Button for larger screens */}
+              <div className="hidden md:block">
+                <Link href={`/search/${searchTerm}`}>
+                  <button
+                    type="submit"
+                    className="px-6 btn border-black text-white fill-white active:scale-95 duration-100 border will-change-transform overflow-hidden relative rounded-xl transition-all"
+                  >
+                    <div className="flex items-center transition-all opacity-1">
+                      <span className="text-sm font-semibold whitespace-nowrap truncate mx-auto">
+                        Search
+                      </span>
+                    </div>
+                  </button>
+                </Link>
+              </div>
+            </label>
+          </form>
+        </div>
       </div>
-    </form>
+    </div>
   );
 };
 
