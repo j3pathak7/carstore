add6522623cfd02e8d1ca64f24ba587d6d792c1b
Jyotirmoy Pathak <jyotirmoypathak.37@gmail.com>
2024-03-29 02:07:37 +0530

    f

--- app/config/firebase.js
+++ app/config/firebase.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -13,8 +12,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-s;
 
 const db = getFirestore(app);
 const storage = getStorage(app);
